957f507bf19e0cc51971f13478e2fbf5fac87320
Mohammed ali kachbouri <med.ali.kachbouri@hotmail.com>
2018-08-26 19:27:58 +0100

    Treating  the case when Tree is null

--- TreeGraphPropagation.Core/TreeGraphPropagation.cs
+++ TreeGraphPropagation.Core/TreeGraphPropagation.cs
@@ -1,19 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace TreeGraphPropagation.Core
 {
-    public class TreeGraphPropagation<TNode> where  TNode : class
+    public class TreeGraphPropagation<TNode> where TNode : class
     {
         private readonly Dictionary<TNode, List<TNode>> Tree;
+
 
         public TreeGraphPropagation(Dictionary<TNode, List<TNode>> tree)
         {
             Tree = tree;
         }
 
+        public struct Root
+        {
+            public TNode RootNode { get; }
+            public int Propagation { get; }
+
+            public Root(TNode rootNode, int propagation)
+            {
+                RootNode = rootNode;
+                Propagation = propagation;
+            }
+        }
+
+        public Root? GetOptimisedRoot()
+        {
+            return null;
+        }
+
     }
+
 }
--- TreeGraphPropagation.Test/TreeGraphPropagationTest.cs
+++ TreeGraphPropagation.Test/TreeGraphPropagationTest.cs
@@ -1,11 +1,23 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using TreeGraphPropagation.Core;
 
 namespace TreeGraphPropagation.Test
 {
     [TestClass]
     public class TreeGraphPropagationTest
     {
-        
+       
+        public class Node
+        {
+            public int NodeId { get; set; }
+        }
+  
+        [TestMethod]
+        public void Should_ReturnEmptyRoot_When_TreeIsEmpty()
+        {
+            var treeGraphPropagation = new TreeGraphPropagation<Node>(null);
+            Assert.IsNull(treeGraphPropagation.GetOptimisedRoot());
+        }
     }
 }
