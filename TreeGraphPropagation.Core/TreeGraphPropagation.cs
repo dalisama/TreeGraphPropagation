51c2f38931c38ec87b15b38595329039808f8ccd
Mohammed ali kachbouri <med.ali.kachbouri@hotmail.com>
2018-08-29 08:52:02 +0100

    Forcing user to implement  IEquatable<TNode>, to quarantie that equal will work properly, Treating the case will a full tree, that has one or two best root

--- TreeGraphPropagation.Core/TreeGraphPropagation.cs
+++ TreeGraphPropagation.Core/TreeGraphPropagation.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TreeGraphPropagation.Core
 {
-    public class TreeGraphPropagation<TNode> where TNode : class
+    public class TreeGraphPropagation<TNode> where TNode : class, IEquatable<TNode>
     {
         private readonly Dictionary<TNode, List<TNode>> Tree;
 
@@ -13,23 +14,44 @@ namespace TreeGraphPropagation.Core
             Tree = tree;
         }
 
-    
+
 
         public Root<TNode>[] GetOptimisedRoot()
         {
-          
+
             if (Tree == null || !Tree.Any()) return null;
             if (Tree.Count == 1) return new Root<TNode>[1] { new Root<TNode>(Tree.Keys.FirstOrDefault(), 0) };
-            if (Tree.Count == 2) return new Root<TNode>[2] { new Root<TNode>(Tree.Keys.FirstOrDefault(), 0) , new Root<TNode>(Tree.Keys.LastOrDefault(), 0) };
+         //   if (Tree.Count == 2) return new Root<TNode>[2] { new Root<TNode>(Tree.Keys.FirstOrDefault(), 1), new Root<TNode>(Tree.Keys.LastOrDefault(), 1) };
 
-         
-            return null;
+
+
+            var indexPropagation = 0;
+            // cloning the tree to keep a working copy
+            var treeCopy = Tree.ToDictionary(x => x.Key, x => x.Value.Select(y => y).ToList());
+            while (treeCopy.Count > 2)
+            {
+                // getting extremum 
+                var extremum = treeCopy.Where(x => x.Value.Count == 1).Select(x => x.Key).ToList();
+                foreach (var item in extremum)
+                {
+                    treeCopy.Remove(item);
+                    foreach (var key in treeCopy.Keys)
+                    {
+                        treeCopy[key].Remove(item);
+                    }
+                }
+                indexPropagation++;
+
+            }
+            if(treeCopy.Count==2) indexPropagation++;
+            return treeCopy.Keys.Select(x => new Root<TNode>(x, indexPropagation)).ToArray();
+          
         }
 
     }
-    public struct Root<TNode> where TNode:class
+    public struct Root<TNode> where TNode : class
     {
-        public TNode RootNode { get;}
+        public TNode RootNode { get; }
         public int Propagation { get; }
 
         public Root(TNode rootNode, int propagation)
@@ -38,4 +60,6 @@ namespace TreeGraphPropagation.Core
             Propagation = propagation;
         }
     }
+
+   
 }
--- TreeGraphPropagation.Test/TreeGraphPropagationTest.cs
+++ TreeGraphPropagation.Test/TreeGraphPropagationTest.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using TreeGraphPropagation.Core;
 
@@ -9,9 +10,33 @@ namespace TreeGraphPropagation.Test
     public class TreeGraphPropagationTest
     {
 
-        public class Node
+        public class Node: IEquatable<Node>
         {
             public int NodeId { get; set; }
+            // override object.Equals
+            public override bool Equals(object obj)
+            {
+
+                if (obj == null || GetType() != obj.GetType())
+                {
+                    return false;
+                }
+
+
+                return ((Node)obj).NodeId == this.NodeId;
+            }
+
+            public bool Equals(Node other)
+            {
+                return other.NodeId == this.NodeId;
+            }
+
+            // override object.GetHashCode
+            public override int GetHashCode()
+            {
+            
+                return base.GetHashCode();
+            }
         }
 
         [TestMethod]
@@ -42,9 +67,53 @@ namespace TreeGraphPropagation.Test
             Assert.IsNotNull(treeGraphPropagation.GetOptimisedRoot());
             var result = treeGraphPropagation.GetOptimisedRoot();
             Assert.AreEqual(result[0].RootNode.NodeId, 1);
-            Assert.AreEqual(result[0].Propagation, 0);
+            Assert.AreEqual(result[0].Propagation, 1);
             Assert.AreEqual(result[1].RootNode.NodeId, 2);
-            Assert.AreEqual(result[1].Propagation, 0);
+            Assert.AreEqual(result[1].Propagation, 1);
+        }
+
+        [TestMethod]
+        public void Should_ReturnOneRoot_When_Tree()
+        {
+            var tree = new Dictionary<Node, List<Node>>();
+            tree[new Node { NodeId = 1 }] = new List<Node> { new Node { NodeId = 2 } };
+            tree[new Node { NodeId = 2 }] = new List<Node> { new Node { NodeId = 1 } , new Node { NodeId = 4 } , new Node { NodeId = 3 } };
+            tree[new Node { NodeId = 3 }] = new List<Node> { new Node { NodeId = 7 }, new Node { NodeId = 8 }, new Node { NodeId = 2 } };
+            tree[new Node { NodeId = 4 }] = new List<Node> { new Node { NodeId = 5 }, new Node { NodeId = 6 }, new Node { NodeId = 2 } };
+            tree[new Node { NodeId = 5 }] = new List<Node> { new Node { NodeId = 4 } };
+            tree[new Node { NodeId = 6 }] = new List<Node> { new Node { NodeId = 4 } };
+            tree[new Node { NodeId = 7 }] = new List<Node> { new Node { NodeId = 3 } };
+            tree[new Node { NodeId = 8 }] = new List<Node> { new Node { NodeId = 3 } };
+            var treeGraphPropagation = new TreeGraphPropagation<Node>(tree);
+            Assert.IsNotNull(treeGraphPropagation.GetOptimisedRoot());
+            var result = treeGraphPropagation.GetOptimisedRoot();
+            Assert.AreEqual(result[0].RootNode.NodeId, 2);
+            Assert.AreEqual(result[0].Propagation, 2);
+      
+        }
+
+        [TestMethod]
+        public void Should_ReturnTwoRoot_When_Tree()
+        {
+            var tree = new Dictionary<Node, List<Node>>();
+            tree[new Node { NodeId = 1 }] = new List<Node> { new Node { NodeId = 2 } };
+            tree[new Node { NodeId = 2 }] = new List<Node> { new Node { NodeId = 1 }, new Node { NodeId = 4 }, new Node { NodeId = 3 } };
+            tree[new Node { NodeId = 3 }] = new List<Node> { new Node { NodeId = 7 }, new Node { NodeId = 8 }, new Node { NodeId = 2 } };
+            tree[new Node { NodeId = 4 }] = new List<Node> { new Node { NodeId = 5 }, new Node { NodeId = 6 }, new Node { NodeId = 2 } };
+            tree[new Node { NodeId = 5 }] = new List<Node> { new Node { NodeId = 4 } };
+            tree[new Node { NodeId = 6 }] = new List<Node> { new Node { NodeId = 4 } };
+            tree[new Node { NodeId = 7 }] = new List<Node> { new Node { NodeId = 3 } };
+            tree[new Node { NodeId = 8 }] = new List<Node> { new Node { NodeId = 3 }, new Node { NodeId = 9 } };
+            tree[new Node { NodeId = 9 }] = new List<Node> { new Node { NodeId = 8 } };
+            var treeGraphPropagation = new TreeGraphPropagation<Node>(tree);
+            Assert.IsNotNull(treeGraphPropagation.GetOptimisedRoot());
+            var result = treeGraphPropagation.GetOptimisedRoot();
+            Assert.AreEqual(result[0].RootNode.NodeId, 2);
+            Assert.AreEqual(result[0].Propagation, 3);
+            Assert.AreEqual(result[1].RootNode.NodeId, 3);
+            Assert.AreEqual(result[1].Propagation, 3);
+
         }
     }
+
 }
